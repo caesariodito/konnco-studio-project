8555c902ef37507ccc685c9ac5e2be577ab44756
caesariodito <caesariodito@gmail.com>
2024-03-06 09:31:40 +0700

    feat: setup db orm with drizzle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    export let data
+    console.log(data.result);
+</script>
+
+<p>If you are here , you successfully setup DRIZZLE ORM with SVELTEKIT</p>
