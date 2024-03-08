7c0752ecba2bbd60a960099e3bed3b65d6118644
caesariodito <caesariodito@gmail.com>
2024-03-08 19:42:17 +0700

    feat(package): added shadcn-svelte

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
 	import { enhance } from "$app/forms";
+	import { Button } from "$lib/components/ui/button"
+	import { Label } from "$lib/components/ui/label"
+	import { Input } from "$lib/components/ui/input"
 </script>
 
-<h1>Sign in</h1>
+<h1 class="bold text-lg text-center">Sign in</h1>
 <form method="post" use:enhance>
-	<label for="username">Username</label>
-	<input name="username" id="username" /><br />
-	<label for="password">Password</label>
-	<input type="password" name="password" id="password" /><br />
-	<button>Continue</button>
+	<Label for="username">Username</Label>
+	<Input name="username" id="username" /><br />
+	<Label for="password">Password</Label>
+	<Input type="password" name="password" id="password" /><br />
+	<Button>
+		<button>
+			Continue
+		</button>
+	</Button>
 </form>
